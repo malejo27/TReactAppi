0b0fe8a908cba86bafe77fe6a5e03ba236e4440d
“estephany2828” <ejargoty@unicauca.edu.co>
2021-12-19 19:36:18 -0500

    fronend y backend

---
+++ backend/demo/src/main/java/com/example/demo/controles/Vistas.java
@@ -0,0 +1,143 @@
+package com.example.demo.controles;
+
+import java.util.List;
+
+import com.example.demo.models.Usuario;
+import com.example.demo.repo.UsuarioRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin
+@RestController
+@RequestMapping("/usuarios")
+public class Vistas {
+
+    //  public static List<Usuario> database = new ArrayList<Usuario>();
+    //  public static int position = -1;
+    //  public static int contador =0;
+
+    // @GetMapping("/")
+
+    // public List<Usuario> index(){
+    //     return Vistas.database;
+    // }
+
+    @Autowired
+    private UsuarioRepo usuarioRepository; 
+
+    @GetMapping("")
+    List<Usuario> index(){
+        return usuarioRepository.findAll(); 
+
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    Usuario create(@RequestBody Usuario usuario){
+        return usuarioRepository.save(usuario);
+
+    }
+    @PostMapping("{id}")
+    Usuario update(@PathVariable String id, @RequestBody Usuario usuario){
+        Usuario usuarioFromDB= usuarioRepository
+        .findById(id)
+        .orElseThrow(RuntimeException::new);
+
+        usuarioFromDB.setNombre(usuario.getNombre());
+        usuarioFromDB.setApellido(usuario.getApellido());
+        usuarioFromDB.setIdentificacion(usuario.getIdentificacion());
+        usuarioFromDB.setCorreo(usuario.getCorreo());
+        usuarioFromDB.setContrasena(usuario.getContrasena());
+        usuarioFromDB.setRol(usuario.getRol());
+        usuarioFromDB.setEstado(usuario.getEstado());
+         
+
+
+        return usuarioRepository.save(usuarioFromDB);
+
+
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    void delete(@PathVariable String id){
+        Usuario usuario= usuarioRepository
+        .findById(id)
+        .orElseThrow(RuntimeException::new);
+
+   
+    usuarioRepository.delete(usuario);
+
+ }
+    // @GetMapping ("/find/{id}")
+    //     public Usuario find(@PathVariable int id){
+    //         this.locate(id);
+    //         if(position < 0 ){
+    //             return null;
+
+    //         }else{
+    //             Usuario usuario = Vistas.database.get(position);
+    //             position = -1;
+    //             return usuario;
+
+    //         }
+
+
+    //     }
+    //     @PostMapping("/update/{id}")
+    //     public Usuario update(@PathVariable int id, @RequestBody Usuario usuario ){
+    //         this.locate(id);
+    //         if(position <0){
+    //             return null;
+    //         }
+    //         else{
+    //             Vistas.database.set(position, usuario);
+    //             position = -1;
+    //             return usuario;
+    //         }
+            
+
+    //     }
+    //     @GetMapping("/delete/{id}")
+    //     public String delete(@PathVariable int id){
+    //         this.locate(id);
+    //         if(position < 0){
+    //             return "Not found for removed";
+
+    //         }else{
+    //             Vistas.database.remove(position);
+    //             position=-1;
+    //             return "Removed id: "+ id;
+    //         }
+
+    //     }
+    //     @PostMapping("/save")
+    //     public Usuario save(@RequestBody Usuario usuario){
+    //         Random rm = new Random();
+    //         usuario.id= rm.nextInt(1000);
+    //         Vistas.database.add(usuario);
+    //         return usuario;
+    //     }
+    
+
+    // public void locate (int id){
+    //     Vistas.database.forEach(data->{
+    //         if(data.id == id){
+    //             position = contador;
+
+    //         }
+    //         contador++;
+
+    //     });
+    //     contador =0;
+    // }
+
+    
+}
---
+++ backend/demo/src/main/java/com/example/demo/models/Usuario.java
@@ -0,0 +1,92 @@
+package com.example.demo.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+// @Document(collection="usuarios")
+@Document
+public class Usuario {
+    @Id
+    public String id;
+  
+    public String nombre;
+    public String apellido;
+    public String identificacion;
+    public String correo;
+    public String contrasena;
+    public String rol;
+    public String estado;
+
+  
+   
+    
+    public Usuario(String id, String nombre, String apellido, String identificacion, String correo, String contrasena,
+            String rol, String estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.identificacion = identificacion;
+        this.correo = correo;
+        this.contrasena = contrasena;
+        this.rol = rol;
+        this.estado = estado;
+    }
+
+    
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getIdentificacion() {
+        return identificacion;
+    }
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public String getContrasena() {
+        return contrasena;
+    }
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    public String getRol() {
+        return rol;
+    }
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+
+    
+}
