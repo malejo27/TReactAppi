5b490e948767e36943b94967311fa7eb002c192d
“estephany2828” <ejargoty@unicauca.edu.co>
2021-12-20 13:21:21 -0500

    mod backend proyectos

---
+++ backend/demo/src/main/java/com/example/demo/models/Rol.java
@@ -0,0 +1,18 @@
+package com.example.demo.models;
+
+public class Rol {
+    public String nombre;
+
+    public Rol(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
